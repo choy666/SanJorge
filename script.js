cc1a719a17d6e8cfa620ef70b82281867cf2021f
Francisco Mingolla <choymingolla@gmail.com>
2023-09-22 19:20:12 -0300

    Planes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// function guardarAlmacenamientoLocal(llave, valor_a_guardar) {
+function guardarAlmacenamientoLocal(llave, valor_a_guardar) {
     // llave seria la direccion de memoria, nombre de acceso a informacion
     // informacion a guardar es el objeto que vamos a guardar en localstorage, lo convierto en json "cadena de texto"
-    // localStorage.setItem(llave, JSON.stringify(valor_a_guardar));
-// }
+    //localStorage.setItem(llave, JSON.stringify(valor_a_guardar));
+}
 // retorno lista de productos 
-// function obtenerAlmacenamientoLocal(llave) {
-    // lista convertidos como objeto js
-//     const listaProductos = JSON.parse(localStorage.getItem(llave));
-//     return listaProductos;
-// }
+function obtenerAlmacenamientoLocal(llave) {
+    //lista convertidos como objeto js
+    //const listaProductos = JSON.parse(localStorage.getItem(llave));
+    //return listaProductos;
+}
 
 //inicializacion
 var checCard1;
@@ -22,11 +22,11 @@ var card4;
 var link_wsp_nro="https://api.whatsapp.com/send?phone=+543834046923h&text= Hola!, estoy interesado en este plan. Como puedo inscribirme?😄.";
 var link_wsp;
 var producto;
-// lista de productos, almacena una lista de productos de lo contrario un array
-// let productos = obtenerAlmacenamientoLocal('productos') || [];
+//lista de productos, almacena una lista de productos de lo contrario un array
+//let productos = obtenerAlmacenamientoLocal('productos') || [];
 
 // let contendorProductos = document.getElementById('contenedorProductos');
-let contendorProductos = document.getElementById('contenedorProductos');
+//let contendorProductos = document.getElementById('contenedorProductos');
 
 function iniciar(){
 
@@ -62,8 +62,8 @@ function iniciar(){
     //                                             <p class="card-text">Valor Nominal: ${productos[i].valorNominal}</p>
     //                                             <div class="alert alert-warning text-center card-precio" role="alert">$${productos[i].valor}</div>
     //                                             <a href="https://wa.link/s905gq" id="${productos[i].id}"><button type="button" class="btn btn-outline-danger col-12">Suscribirme</button>
-    //                                         </div>
-    //                                     </div>`;
+    //                                          </div>
+    //                                      </div>`;
     //     link_wsp = document.getElementById(productos[i].id);
     //     producto=productos[i].urlImagen;
     //     link_wsp.setAttribute("href",link_wsp_nro+" Codigo: "+productos[i].id+" Nombre: "+productos[i].nombre+" "+producto);
